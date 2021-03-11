4499f0a81996872da12e2dbedc9d161ca93225c5
jrdefideveloper <67920778+jrdefideveloper@users.noreply.github.com>
2021-03-10 23:36:01 -0800

    Update MultiSigWallet.sol

--- MultiSigWallet.sol
+++ MultiSigWallet.sol
@@ -15,9 +15,7 @@ pragma abicoder v2;
 contract MultiSigWallet { 
     address[] public approvalGivers;
     uint signaturesRequired; // number of approvals required per transfer 
-    
-    //["0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2", "0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db"]
-    
+        
     struct Transfer { 
         uint amount; 
         address payable receiver; 
