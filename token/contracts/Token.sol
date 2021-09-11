6fd17f95c03ac00714775158e31bf04cfedc13d7
jrdefideveloper <ecom.jessicaju@gmail.com>
2021-09-10 21:37:28 -0700

    Ignore build directory

--- token/contracts/Token.sol
+++ token/contracts/Token.sol
@@ -7,8 +7,9 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract MyToken is Ownable, ERC20PresetMinterPauser {
 
-    // Using ERC20PresetMinterPauser's roles {minter, pauser}
     bytes32 public constant CAPPER_ROLE = keccak256("CAPPER_ROLE");
+    using SafeMath for uint256;
+    uint256 private _cap;
 
     constructor() ERC20PresetMinterPauser("RandomCoin", "RC") {
         _cap = 1000;
@@ -16,34 +17,22 @@ contract MyToken is Ownable, ERC20PresetMinterPauser {
         _mint(msg.sender, 10);
     }
 
-    using SafeMath for uint256;
-    uint256 private _cap;
-
     function cap() public view virtual returns (uint256) {
         return _cap;
     }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal virtual override {
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override {
         super._beforeTokenTransfer(from, to, amount);
-
         if (from == address(0)) {
-            require(
-                totalSupply().add(amount) <= cap(), "ERC20Capped: cap exceeded"
-            );
+            require(totalSupply().add(amount) <= cap(), "ERC20Capped: cap exceeded");
         }
     }
 
-    // new function that allows you to modify the cap.
     function setCap(uint256 _newCap) public {
-        require(
-            hasRole(CAPPER_ROLE, _msgSender()),
-            "ERC20PresetMinterPauser: must have capper role to change cap"
-        );
-        require(_newCap > 0, "ERC20Capped: cap is 0");
+        string memory setCapErrorMessage = "setCap(): must have CAPPER_ROLE to change cap"; 
+        require(hasRole(CAPPER_ROLE, _msgSender()), setCapErrorMessage);
+        string memory capMustBeGreaterThanZeroErrorMessage = "setCap(): incorrect cap amount set, must be greater than zero"; 
+        require(_newCap > 0, capMustBeGreaterThanZeroErrorMessage);
         _cap = _newCap;
     }
 }
