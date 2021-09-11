7775e121f6635ba0a52a8fb31a7cf3cda5b1ee8c
jrdefideveloper <ecom.jessicaju@gmail.com>
2021-09-10 21:16:47 -0700

    ERC20 token that has 3 roles and allows for setting cap, mint, and pause

--- token/contracts/Token.sol
+++ token/contracts/Token.sol
@@ -1,9 +1,49 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
-import "../node_modules/@openzeppelin/contracts/token/ERC20/Extensions/ERC20Capped.sol"; 
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol"; 
 
-contract MyToken is ERC20Capped, Ownable { 
-    constructor() ERC20("MyToken", "MT") ERC20Capped(100000) { 
-        ERC20._mint(msg.sender, 1000); 
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol"; 
+
+contract MyToken is Ownable, ERC20PresetMinterPauser {
+
+    // Using ERC20PresetMinterPauser's roles {minter, pauser}
+    bytes32 public constant CAPPER_ROLE = keccak256("CAPPER_ROLE");
+
+    constructor() ERC20PresetMinterPauser("RandomCoin", "RC") {
+        _cap = 1000;
+        _setupRole(CAPPER_ROLE, _msgSender());
+        _mint(msg.sender, 10);
+    }
+
+    using SafeMath for uint256;
+    uint256 private _cap;
+
+    function cap() public view virtual returns (uint256) {
+        return _cap;
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        super._beforeTokenTransfer(from, to, amount);
+
+        if (from == address(0)) {
+            require(
+                totalSupply().add(amount) <= cap(), "ERC20Capped: cap exceeded"
+            );
+        }
+    }
+
+    // new function that allows you to modify the cap.
+    function setCap(uint256 _newCap) public {
+        require(
+            hasRole(CAPPER_ROLE, _msgSender()),
+            "ERC20PresetMinterPauser: must have capper role to change cap"
+        );
+        require(_newCap > 0, "ERC20Capped: cap is 0");
+        _cap = _newCap;
     }
 }
